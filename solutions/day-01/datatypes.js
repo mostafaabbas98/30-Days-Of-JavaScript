aaddf44b3052f1ac7151ffa623dc61e517af63e8
Mostafa Abbas <mostafaabbas2902@gmail.com>
2022-07-25 18:34:06 +0200

    exercise level1 complete

---
+++ solutions/day-01/datatypes.js
@@ -0,0 +1,10 @@
+let str = 'Mostafa';
+console.log(typeof str);
+let num = 123;
+console.log(typeof num);
+let bol = true;
+console.log(bol);
+let noval;
+console.log(typeof noval);
+let empty = null;
+console.log(empty);
---
+++ solutions/day-01/variable.js
@@ -0,0 +1,5 @@
+let name = 'Mostafa Abbas';
+let age = 24;
+let handsome = true;
+let varible;
+let empty = null;
